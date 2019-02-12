066797f29703d22ec67d33d4d886d61666618660
mohsencomp <mohsencomp90@gmail.com>
2019-02-12 22:33:39 +0330

    add text

--- app/src/main/java/com/example/parssystem/arclayout/MainActivity.java
+++ app/src/main/java/com/example/parssystem/arclayout/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.parssystem.arclayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this is a content
         setContentView(R.layout.layout2);
     }
 }
